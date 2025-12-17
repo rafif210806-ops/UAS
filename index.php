ba38f9b3fee6f8fb41b7fd09a81baa48eaf51e7a
rapip <rafif210806@gmail.com>
2025-12-17 12:53:29 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand w-100 text-center" href="#">
+      Politeknik Negeri Medan
+    </a>
+
+    <button class="navbar-toggler position-absolute end-0 me-3"
+      type="button"
+      data-bs-toggle="collapse"
+      data-bs-target="#navbarNavAltMarkup"
+      aria-controls="navbarNavAltMarkup"
+      aria-expanded="false"
+      aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+  </div>
+</nav>
+
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-link active" aria-current="page" href="#">Home</a>
+        <a class="nav-link" href="#">Tentang</a>
+        <a class="nav-link" href="#">Data Mahasiwa</a>
+      </div>
+    </div>
+  </div>
+</nav>
+<div class="mx-auto p-2 bg-danger" style="width: 200px;"> Politeknik Negeri Medan</div>
+<img src="image.png" class="img-fluid" alt="...">
+<div class="sejarah-container">
+    <h2>Sejarah Politeknik Negeri Medan</h2>
+
+    <p>
+        Pada tahun 1979 Universitas Sumatera Utara ditetapkan sebagai salah satu dari
+        enam Universitas/Institut yang memperoleh proyek pembangunan Pendidikan
+        Politeknik Tahap I dari DIKTI dengan nama Politeknik Universitas Sumatera Utara
+        (Politeknik USU). Pembangunan ini dibiayai oleh World Bank.
+    </p>
+
+    <p>
+        Pada tahap pertama, pendidikan yang dikembangkan adalah bidang keteknikan
+        dengan dukungan tenaga ahli dari Swiss yang ditempatkan di Medan.
+        Selanjutnya pada tahap kedua dibangun pendidikan bidang tata niaga yang
+        didukung oleh tenaga ahli dan fasilitas dari Australia. Program ini mulai
+        diselenggarakan pada tahun 1986.
+    </p>
+
+    <p>
+        Pembangunan Politeknik USU dan politeknik lainnya merupakan proyek pemerintah
+        melalui Depdikbud Ditjen DIKTI yang dilaksanakan oleh Polytechnic Education
+        Development Center (PEDC) di Bandung.
+    </p>
+
+    <p>
+        Melalui Surat Keputusan Mendikbud Nomor 084/O/1997 tentang pendirian
+        Politeknik Negeri Medan, Politeknik USU secara resmi menjadi politeknik
+        mandiri dengan nama Politeknik Negeri Medan. Ketentuan ini diperbaharui
+        melalui Surat Keputusan Mendiknas Nomor 130/O/2002 tentang organisasi dan
+        tata kerja Politeknik Negeri Medan.
+    </p>
+
+    <p class="lokasi">
+        Walaupun telah berdiri sendiri, Politeknik Negeri Medan masih menempati
+        kampus Universitas Sumatera Utara yang beralamat di Jalan Almamater No. 1
+        Kampus USU Medan atau melalui Jalan Tridharma No. 2 Kampus USU (Pintu 4).
+    </p>
+</div>
+</body>
+</html>
